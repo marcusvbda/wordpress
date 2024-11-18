b54b7eecf18bf35f40fd2c1aebdc2ddee83e0c8f
Vinicius Bassalobre <bassalobre.vinicius@gmail.com>
2024-11-18 19:32:51 -0300

    first commit

---
+++ init.sql
@@ -0,0 +1,31 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost:3306
+-- Tempo de geração: 02-Out-2024 às 14:23
+-- Versão do servidor: 5.5.68-MariaDB
+-- versão do PHP: 8.3.9
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Banco de dados: `sgw_aneps`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `associados_certificados`
+--
+
+CREATE DATABASE IF NOT EXISTS db_wordpress;
+GRANT ALL PRIVILEGES ON db_wordpress.* TO 'user'@'%';
